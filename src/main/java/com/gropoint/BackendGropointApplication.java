35781bb8302dbe340a4f2e57dcfea1588354a474
Muhammad Anies WAhdie <manies5396@gmail.com>
2022-01-18 22:40:02 +0700

    First commit

---
+++ src/main/java/com/gropoint/BackendGropointApplication.java
@@ -0,0 +1,13 @@
+package com.gropoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGropointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGropointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gropoint/BackendGropointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gropoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGropointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
